2082c3ad3ff7c684dd3669175486b70c8a179ca9
Ankit Rajput <ankitrajput1902@gmail.com>
2022-12-04 22:35:10 +0530

    wip

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ import "./App.css";
 export default function App() {
   return (
     <div id="App">
-      {process.env.NODE_ENV === "production" ? (
-        <ComingSoon />
-      ) : (
+      {process.env.NODE_ENV === "development" ? (
         <div>
           {" "}
           <SideBar /> <Menu />
         </div>
+      ) : (
+        <ComingSoon />
       )}
       <div id="page-wrap"></div>
     </div>
